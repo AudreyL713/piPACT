382237570a6ea13b34068d9ad32e299d1ecfbdf0
AudreyL713 <2021leea@gmail.com>
2020-07-03 04:35:33 -0700

    enable testing on sensors.py

--- sensors.py
+++ sensors.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from time import sleep, strftime, time
+from csv import writer
 import pi_pact
 import board
 import dht
@@ -7,39 +8,50 @@ import bmp
 import statistics
 
 # Initial the dht device, with data pin connected to:
-hSensor = dht.DHT11(board.D18)
-pSensor = bmp.BMP085()
+# hSensor = dht.DHT11(board.D18)
+# pSensor = bmp.BMP085()
 scanner = pi_pact.Scanner_Blank(control_file_name="scanner_control")
 
-humidityList = list()
-temperatureList = list()
-pressureList = list()
 rssiList = list()
+# humidityList = list()
+# temperatureList = list()
+# pressureList = list()
 
-def write_data(val1, val2, val3):
-    with open("/home/pi/Documents/log.csv", "a") as log:
-        log.write("{0},{1},{2},{3}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(val1), str(val2), str(val3)))
+def write_data(data_list):
+    # with open("/home/pi/Documents/log.csv", "a+" newline='') as log:
+    with open("/home/aud2pact/piPACT/pact_scans/log.csv", "a+" newline='') as log:
+        csv_writer = writer(log)
+        csv_writer.writerow(data_list)
+        # log.write("{0},{1},{2},{3}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(val1), str(val2), str(val3)))
 
 while True:
     for i in range(10):
         rssiList.append(scanner.return_RSSI())
-        humidityList.append(hSensor.humidity())
-        temperatureList.append(pSensor.read_temperature())
-        pressureList.append(pSensor.read_pressure())
+        # humidityList.append(hSensor.humidity())
+        # temperatureList.append(pSensor.read_temperature())
+        # pressureList.append(pSensor.read_pressure())
         print(rssiList)
-        print(humidityList)
-        print(temperatureList)
-        print(pressureList)
+        # print(humidityList)
+        # print(temperatureList)
+        # print(pressureList)
         print()
         sleep(2.0)
-    humidity = statistics.mean(humidityList)
-    temperature = statistics.mean(temperatureList)
-    pressure = statistics.mean(pressureList)
+    # rssi = statistics.mean(rssiList)
+    # humidity = statistics.mean(humidityList)
+    # temperature = statistics.mean(temperatureList)
+    # pressure = statistics.mean(pressureList)
 
-    write_data(temperature, humidity, pressure)
+    data_list = list()
+    data_list.append(statistics.mean(rssiList))
+    # data_list.append(statistics.mean(humidityList))
+    # data_list.append(statistics.mean(temperatureList))
+    # data_list.append(statistics.mean(pressureList))
 
-    humidityList.clear()
-    temperatureList.clear()
-    pressureList.clear()
+    write_data(data_list)
+
+    rssiList.clear()
+    # humidityList.clear()
+    # temperatureList.clear()
+    # pressureList.clear()
 
     sleep(30.0)
