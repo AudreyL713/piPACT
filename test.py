33b66250d837b8b5ba033458a400c39a8f8b75ed
AudreyL713 <2021leea@gmail.com>
2020-07-03 02:33:15 -0700

    trying to fix testing

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 from time import sleep, strftime, time
 import pi_pact
-import board
-import dht
-import bmp
+# import board
+# import dht
+# import bmp
 import statistics
 
 scanner = pi_pact.Scanner_Blank(control_file="scanner_control")
