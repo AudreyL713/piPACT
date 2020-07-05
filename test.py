fd31c71ad6f3d394b379016c3410b499b363a999
AudreyL713 <2021leea@gmail.com>
2020-07-05 16:32:43 -0700

    attempt to test bme280

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ import busio
 import adafruit_bme280
 import statistics
 
-scanner = pi_pact.Scanner_Blank(control_file_name="scanner_control")
+i2c = busio.I2C(board.SCL, board.SDA)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
-print(scanner.return_RSSI())
+# scanner = pi_pact.Scanner_Blank(control_file_name="scanner_control")
+
+print(bme280.temperature)
