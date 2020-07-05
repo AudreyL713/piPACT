af0dcb7bf080c222b4632a0611032aa9784b07eb
AudreyL713 <2021leea@gmail.com>
2020-07-05 15:41:35 -0700

    put in correct imports

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 from time import sleep, strftime, time
 import pi_pact
-# import board
-# import dht
-# import bmp
+import board
+import busio
+import adafruit_bme280
 import statistics
 
 scanner = pi_pact.Scanner_Blank(control_file_name="scanner_control")
