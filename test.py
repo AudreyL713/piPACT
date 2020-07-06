423b064b2736135dc0069affea6395dbe063d2a3
AudreyL713 <2021leea@gmail.com>
2020-07-05 17:45:43 -0700

    calibration

--- test.py
+++ test.py
@@ -8,6 +8,13 @@ import statistics
 
 i2c = busio.I2C(board.SCL, board.SDA)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+bme280.sea_level_pressure = 1012.8689
+bme280.mode = adafruit_bme280.MODE_NORMAL
+bme280.standby_period = adafruit_bme280.STANDBY_TC_500
+bme280.iir_filter = adafruit_bme280.IIR_FILTER_X16
+bme280.overscan_pressure = adafruit_bme280.OVERSCAN_X16
+bme280.overscan_humidity = adafruit_bme280.OVERSCAN_X1
+bme280.overscan_temperature = adafruit_bme280.OVERSCAN_X2
 
 # scanner = pi_pact.Scanner_Blank(control_file_name="scanner_control")
 
